facd2d00b42c7626a0fc90ce2f590fea06fc1cb5
Edward <hua9939@gmail.com>
2023-12-10 21:18:42 +1300

    add content(name, role and images)

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
-# This is a sample Python script.
+import streamlit as st
+import pandas
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")
+
+#add header and some other text
+st.header("The Best Company")
+content1="""Determining the "best" company can be subjective and depends on 
+various factors such as industry, values, financial performance, and more. 
+However, I can provide descriptions of a few companies that are often considered 
+among the best in their respective fields as of my last knowledge update 
+in January 2022. Please note that the status of companies can change, so it's a 
+good idea to check the latest information.
+"""
+st.write(content1)
+
+st.header("Our Team")
+
+df = pandas.read_csv("data.csv", sep=",")
+# prepare columns
+col1, col2, col3 = st.columns(3)
+
+# add content in each column
+with col1:
+    for index, row in df[0:4].iterrows():
+        name = f"{row["first name"]} {row["last name"]}".title()
+        st.header(name)
+        st.write(row["role"])
+        st.image(f"images/{row["image"]}")
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        name = f"{row["first name"]} {row["last name"]}".title()
+        st.header(name)
+        st.write(row["role"])
+        st.image(f"images/{row["image"]}")
+
+with col3:
+    for index, row in df[8:].iterrows():
+        name = f"{row["first name"]} {row["last name"]}".title()
+        st.header(name)
+        st.write(row["role"])
+        st.image(f"images/{row["image"]}")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
